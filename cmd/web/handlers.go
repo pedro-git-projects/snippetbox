b2d7b727c82ee7f42b7859d29cd7bc41107b5c30
pedro-git-projects <pedro.generic.email@gmail.com>
2022-07-03 20:21:48 -0300

    templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, snippetbox"))
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
+	}
+
+	files := []string{
+		"ui/html/home.page.tmpl",
+		"ui/html/base.layout.tmpl",
+		"ui/html/footer.partial.tmpl",
+	}
+
+	// template.ParseFiles will turn the templates into a template set
+	ts, err := template.ParseFiles(files...)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// Execute, in its turn will write the contents of a template set into a response body
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
 	}
 }
 
--- notes.md
+++ notes.md
@@ -1,1 +1,30 @@
+# Notas - 2.8
+## Template Engine 
 
+A linguagem Go  possui uma template engine já integrada em suas bibliotecas padrão.
+
+Para criar um template criamos um arquivo de nome *nome*.*papel*.tmpl. Tal que os papéis são *page*, *partial* e *layout*.    
+
+Para manter o templates DRY, basta utilizarmos o sistema de ações. Por exemplo, para definir o layout base da página basta:
+
+```go
+{{define "base"}}
+{{end}}
+```
+
+Para invocar uma ação pré-definida, neste caso base, utilizamos:
+
+```go
+{{template "base" .}}
+```
+
+Para ler um template basta na função handler correspondente:
+
+```go
+files := []string{
+	// lista de templates 
+}
+ts, err := template.ParseFiles(files...)
+// tratamento de erros
+err = ts.Execute(w, nil)
+```
