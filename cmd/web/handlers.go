e9efe9fa1d8e98ea324fef89eab971a8fdb13033
pedro-git-projects <pedro.generic.email@gmail.com>
2023-01-08 19:53:22 -0300

    validator struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,18 +6,17 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/pedro-git-projects/snippetbox/internal/models"
+	"github.com/pedro-git-projects/snippetbox/internal/models/validator"
 )
 
 type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 func (app *application) render(w http.ResponseWriter, status int, page string, data *templateData) {
@@ -99,27 +98,17 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	}
 
 	form := snippetCreateForm{
-		Title:       r.PostForm.Get("title"),
-		Content:     r.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: map[string]string{},
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
 	}
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "Required field"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "Exceeded 100 character limit"
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title", "Required Field")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "Exceeded 100 Character Limit")
+	form.CheckField(validator.NotBlank(form.Content), "content", "Required Field")
+	form.CheckField(validator.IntWithinSet(form.Expires, 1, 7, 365), "expires", "value outside range")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "Required field"
-	}
-
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		form.FieldErrors["expires"] = "Value must be 1, 7 or 365"
-	}
-
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
